72e7fd03dd35b6101b0b41ae644d08836a92383b
Carl Frederik Grønvald <bonezaux@gmail.com>
2020-03-03 11:26:44 +0100

    updated u search and added figure saving

--- bolusDiabetes.py
+++ bolusDiabetes.py
@@ -40,7 +40,6 @@ def phi(x0, u0, d0, parm):
     totalXs = np.concatenate((xs1[:,3], xs[:,3]))
     plt.plot(totalTs,totalXs)
     return phi
-    
 
 #Problem 7
 def findOptimalU(x0, d0, parm):
@@ -48,33 +47,36 @@ def findOptimalU(x0, d0, parm):
     
     
     #Do u estimates
-    precision = 10000
-    minU = 100000
+    precision = 5000
+    minU = 10000
     
-    for loop in range(0,5):
+    for loop in range(0,15):
         phis = []
-        Us = [i*precision+minU for i in range(-11,11)]
+        Us = [(i)*precision+minU for i in range(-1,2)]
         for U in Us:
             phis += [phi(x0,U,d0,parm)]
         
-        plt.ylabel("Blood glucose")
-        plt.xlabel("Time [min]")
-        plt.show()
-        plt.ylabel("phi")
-        plt.xlabel("bolus [mU]")
-        plt.plot(Us,phis)
-        plt.show()
+        #plt.ylabel("phi")
+        #plt.xlabel("bolus [mU]")
+        #plt.plot(Us,phis)
+        #plt.show()
     
         #Then, do finer estimate of minimum
         minU = Us[np.argmin(phis)]
         
-        precision /= 10
+        precision /= 2
     
     #Then, this is the best U
     optimalU = minU
         
+    plt.ylabel("Blood glucose")
+    plt.xlabel("Time [min]")
+    plt.title("Bolus guesses, d0=" + str(d0) + "g carbs")
+    plt.savefig("bolusguess"+str(d0)+".svg", format="svg")
+    plt.show()
     plt.ylabel("phi")
     plt.xlabel("bolus [mU]")
+    plt.title("Bolus size vs carbs in meal")
     plt.plot(Us,phis)
     plt.show()
     
@@ -97,7 +99,7 @@ def Problem8(stepSize):
     }
     x0 = [1.2458,1.2458,0.0101,108.2115,0,0]
     
-    d0s = np.arange(100,150,stepSize)
+    d0s = np.arange(70,200,stepSize)
     optimalUs = []
     for d0 in d0s:
         optimalUs += [findOptimalU(x0, d0, parm)]
@@ -113,13 +115,14 @@ def Problem9():
     c = np.polyfit(d0s, optimalUs, 2)
     
     
-    ds = np.linspace(50,169,300);
+    ds = np.linspace(60,200,300);
     p = np.poly1d(c)
     fitPoly = p(ds)
     
     plt.plot(ds,fitPoly)
     plt.plot(d0s,optimalUs, 'o')
-    plt.ylabel("Bolus")
+    plt.ylabel("Bolus [mU]")
     plt.xlabel("Carbs in meal [g]")
+    plt.savefig("bolusplot.svg", format="svg")
     plt.show()
     
